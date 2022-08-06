f0a36c81a6638976f14e02c4e2aa52d65b9dd03c
ZEID <zeidcode@gmail.com>
2022-08-06 16:40:47 +0400

    Example_005ifelse

---
+++ Example_005(If,else)/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "fanny")
+
+{
+    Console.WriteLine("Yahoo Hello Fanny");
+}
+else
+{
+    Console.WriteLine("Hello random " );
+    Console.Write(username);
+}
